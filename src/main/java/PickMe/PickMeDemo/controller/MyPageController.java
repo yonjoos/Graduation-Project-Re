3a4354aea85b7dfb9bc9a0f3514e3e93302f3e39
Fresh-hongsi <qkrtlghd97@naver.com>
2023-08-25 17:04:16 +0900

    myPage완성

--- src/main/java/PickMe/PickMeDemo/controller/MyPageController.java
+++ src/main/java/PickMe/PickMeDemo/controller/MyPageController.java
@@ -1,7 +1,9 @@
 package PickMe.PickMeDemo.controller;
 
+import PickMe.PickMeDemo.dto.SignOutDto;
 import PickMe.PickMeDemo.dto.UserBaseInfoUpdateDto;
 import PickMe.PickMeDemo.dto.UserDto;
+import PickMe.PickMeDemo.dto.UserPasswordUpdateDto;
 import PickMe.PickMeDemo.exception.AppException;
 import PickMe.PickMeDemo.service.UserService;
 import jakarta.validation.Valid;
@@ -18,48 +20,60 @@ public class MyPageController {
 
     private final UserService userService;
 
-    //유저 정보 가져오기 관련
+    //user의 기본정보 가져오기 관련
     @GetMapping("/userInfo")
     public ResponseEntity<UserDto> userInfo(Principal principal) {
-        String userEmail = principal.getName(); // Get the email from the JWT token
+        String userEmail = principal.getName(); // jwt토큰을 기반으로 user의 email을 찾는다.
 
-        //System.out.println(userEmail);
-        //UserDto userDto=(UserDto) principal;
-        // Retrieve user information from the userService based on the email
-        UserDto userDto = userService.findByEmail(userEmail);
+        UserDto userDto = userService.findByEmail(userEmail); //user의 email을 기반으로 user의 정보를 userDto로 변환해 가져온다
 
-        return ResponseEntity.ok(userDto);
+        return ResponseEntity.ok(userDto); //userDto를 프론트에 반환한다.
     }
 
-    //닉네임, 이름 변경 관련
+    //user의 닉네임, 이름 변경 관련
     @PutMapping("/updateUserInfo")
     public ResponseEntity<String> updateUserInfo(@RequestBody @Valid UserBaseInfoUpdateDto updateDto, Principal principal) {
-        String userEmail = principal.getName();
+        String userEmail = principal.getName(); // jwt토큰을 기반으로 user의 email을 찾는다.
 
         try {
-            // Call a method in your userService to handle the user base info update logic
-            userService.updateUserBaseInfo(userEmail, updateDto);
+            userService.updateUserBaseInfo(userEmail, updateDto); //email기반으로 user을 찾고, 입력받은 비밀번호가 db의 비밀번호와 같다면 닉네임 또는 이름 업데이트 진행
             return ResponseEntity.ok("User information has been successfully updated.");
         } catch (AppException ex) {
-
             return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
         }
     }
 
 
-
-    //회원탈퇴 관련
-    @PostMapping("/signOut")
-    public ResponseEntity<String> signOut(Principal principal) {
-        String userEmail = principal.getName(); // Get the email from the JWT token!
+    //user의 비밀번호 변경 관련
+    @PutMapping("/updatePassword")
+    public ResponseEntity<String> updatePassword(@RequestBody @Valid UserPasswordUpdateDto userPasswordUpdateDto, Principal principal){
+        String userEmail = principal.getName(); // jwt토큰을 기반으로 user의 email을 찾는다.
 
         try {
-            // Call a method in your userService to handle the user withdrawal logic
-            userService.signOut(userEmail);
+            //email기반으로 user을 찾고, 입력받은 기존의 비밀번호가 db의 비밀번호와 같다면 바꾸려는 비밀번호로 user의 password를 업데이트한다.
+            userService.updateUserPassword(userEmail, userPasswordUpdateDto.getCurrentPassword(),userPasswordUpdateDto.getPassword());
+            return ResponseEntity.ok("Password updated successfully.");
+        } catch (AppException ex) {
+            return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
+        }
+
+
+    }
+
+
+
+    //user의 회원탈퇴 관련
+    @PostMapping("/signOut")
+    public ResponseEntity<String> signOut(@RequestBody @Valid SignOutDto signOutDto, Principal principal) {
+        String userEmail = principal.getName(); // jwt토큰을 기반으로 user의 email을 찾는다.
+
+        try {
+            //email기반으로 user을 찾고, 입력받은 기존의 비밀번호가 db의 비밀번호와 같다면 회원탈퇴를 진행한다
+            userService.signOut(userEmail, signOutDto.getCurrentPasswordForSignOut() );
             return ResponseEntity.ok("User has been successfully withdrawn.");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to withdraw user.");
+        } catch (AppException ex) {
+
+            return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
         }
     }
 }
--- src/main/java/PickMe/PickMeDemo/service/UserService.java
+++ src/main/java/PickMe/PickMeDemo/service/UserService.java
@@ -1,9 +1,6 @@
 package PickMe.PickMeDemo.service;
 
-import PickMe.PickMeDemo.dto.CredentialsDto;
-import PickMe.PickMeDemo.dto.SignUpDto;
-import PickMe.PickMeDemo.dto.UserBaseInfoUpdateDto;
-import PickMe.PickMeDemo.dto.UserDto;
+import PickMe.PickMeDemo.dto.*;
 import PickMe.PickMeDemo.entity.Portfolio;
 import PickMe.PickMeDemo.entity.Role;
 import PickMe.PickMeDemo.entity.User;
@@ -59,7 +56,7 @@ public class UserService {
 
         // 비밀번호 필드를 제외한 나머지 부분 채우기
         User user = userMapper.signUpToUser(userDto);
-        
+
         // 암호 인코더를 사용하여 암호를 일반 텍스트로 저장하지 않고, 해싱함.
         // 응용 프로그램에서 암호로 작업할 때 해싱은 매우 중요함.
         // 비밀번호는 따로 해싱하여 세팅
@@ -87,10 +84,18 @@ public class UserService {
     }
 
 
-    public void signOut(String userEmail) {
-        // userEmail로 user 찾기
+    @Transactional
+    public void signOut(String userEmail, String currentPasswordForSignOut) {
+        // userEmail에 해당하는 user를 찾기
         User user = userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new AppException("User not found", HttpStatus.NOT_FOUND));
+
+        // SignOutDto에서 가져온 기존 비밀번호를 암호화해보기
+        // user 테이블의 암호화되어 저장되어있는 비밀번호와 currentPasswordForSignOut 암호화 한 값이 같은 경우, 탈퇴 가능
+        // 만약 두 값이 다르다면 비밀번호 비밀번호가 틀린 것이므로 예외 반환
+        if (!passwordEncoder.matches(currentPasswordForSignOut, user.getPassword())) {
+            throw new AppException("Current password is incorrect", HttpStatus.BAD_REQUEST);
+        }
 
         // 회원의 포트폴리오 찾기
         Portfolio portfolio = user.getPortfolio();
@@ -110,25 +115,50 @@ public class UserService {
 
         // userEmail에 해당하는 user를 찾기
         User user = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new AppException("User not found",HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new AppException("User not found", HttpStatus.NOT_FOUND));
 
         // updateDto에서 가져온 비밀번호를 암호화
-        // user 테이블의 암호화되어 저장되어있는 비밀번호가 같은 경우
+        // user 테이블의 암호화되어 저장되어있는 비밀번호가 같은 경우, 닉네임 또는 이름 변경 가능(닉네임은 중복 불가능하므로 닉네임이 중복되면 예외 반환)
         if (passwordEncoder.matches(CharBuffer.wrap(updateDto.getPassword()), user.getPassword())) {
 
             // 변경하려는 닉네임이 이미 다른 사용자에게서 사용되는지 확인
             Optional<User> existingUserWithNickname = userRepository.findByNickName(updateDto.getNickName());
 
             //만약 같은 닉네임을 쓰는 회원이 존재하고, 기존에 해당 닉네임을 쓰는 사람이 현재 같은 닉네임으로 바꾸려는 사람의 pk가 다르다면, 이를 거부
-            if (existingUserWithNickname.isPresent() && !existingUserWithNickname.get().getId().equals(user.getId())) {
-                throw new AppException("Nickname already in use", HttpStatus.CONFLICT);
-            }
+//            if (existingUserWithNickname.isPresent() && !existingUserWithNickname.get().getId().equals(user.getId())) {
+//                throw new AppException("Nickname already in use", HttpStatus.CONFLICT);
+//            }
 
-            user.setNickName(updateDto.getNickName());
-            user.setUserName(updateDto.getUserName());
-            userRepository.save(user); //변경감지 기능 통해 업데이트 되게
+            //만약 같은 닉네임을 쓰는 회원이 존재하고, 기존에 해당 닉네임을 쓰는 사람이 현재 같은 닉네임으로 바꾸려는 사람의 pk가 다르다면, 이를 거부
+            //위의 코드를 아래 코드로 리팩터링
+            existingUserWithNickname.ifPresent(existingUser -> {
+                if (!existingUser.getId().equals(user.getId())) {
+                    throw new AppException("Nickname already in use", HttpStatus.CONFLICT);
+                }
+            });
+
+            user.setNickName(updateDto.getNickName()); //변경 감지에 의해 닉네임이 변경되도록 함
+            user.setUserName(updateDto.getUserName()); //변경 감지에 의해 이름이 변경되도록 함
+            userRepository.save(user); //변경감지 기능 통해 업데이트
         } else {
             throw new AppException("Passwords do not match", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @Transactional
+    public void updateUserPassword(String userEmail, String currentPassword, String password) {
+        //userEmail에 해당하는 회원 찾기
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new AppException("User not found",HttpStatus.NOT_FOUND));
+
+        // UserPasswordUpdateDto에서 가져온 기존 비밀번호를 암호화해보기
+        // user 테이블의 암호화되어 저장되어있는 비밀번호와 currentPassword를 암호화 한 값이 같은 경우, 비밀번호 변경 가능
+        // 만약 두 값이 다르다면 비밀번호 비밀번호가 틀린 것이므로 예외 반환
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            throw new AppException("Current password is incorrect", HttpStatus.BAD_REQUEST);
+        }
+        // 패스워드 변경
+        user.setPassword(passwordEncoder.encode(CharBuffer.wrap(password))); //변경 감지에 의해 패스워드가 변경되도록 함
+        userRepository.save(user); //변경감지 기능 통해 업데이트
     }
 }

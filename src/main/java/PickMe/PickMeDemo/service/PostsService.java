6048da50a3be3f1a8170316957d7dfa0086c17b8
Fresh-hongsi <qkrtlghd97@naver.com>
2023-08-30 23:55:57 +0900

    project page search 기능 완성

--- src/main/java/PickMe/PickMeDemo/service/PostsService.java
+++ src/main/java/PickMe/PickMeDemo/service/PostsService.java
@@ -6,6 +6,7 @@ import PickMe.PickMeDemo.exception.AppException;
 import PickMe.PickMeDemo.repository.CategoryRepository;
 import PickMe.PickMeDemo.repository.PostsRepository;
 import PickMe.PickMeDemo.repository.UserRepository;
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -19,11 +20,13 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -392,31 +395,82 @@ public class PostsService {
     
     //게시물 조회 동적쿼리 + 페이징 in 프로젝트 게시물
     @Transactional(readOnly = true) //읽기 전용
-    public Page<PostsListDto> getFilteredProjects(List<String> selectedBanners, String sortOption, Pageable pageable) {
+    public Page<PostsListDto> getFilteredProjects(List<String> selectedBanners, String sortOption, String searchTerm, Pageable pageable) {
 
         QPosts posts = QPosts.posts;
         QCategory category = QCategory.category;
 
         //System.out.println("pageable.getOffset() = " + pageable.getOffset());
         //System.out.println("pageable.getPageSize() = " + pageable.getPageSize());
+        //System.out.println("searchTerm = " + searchTerm);
+        //System.out.println("searchTerm = " + searchTerm.getClass());
 
         // buildBannerConditionsInProjects 메서드를 사용하여 선택한 배너를 기반으로 BooleanExpression을 구성
         BooleanExpression bannerConditions = buildBannerConditionsInProjects(category, selectedBanners);
 
-        // 데이터를 가져오는 쿼리
+        // 검색어 기반으로 필터링할 때 쓰는 BooleanExpression 조건
+        BooleanExpression titleOrContentConditions = null;
+
+//        아래 주석은 공백 기호 and조건 없이 그냥 단순하게 찾는 기법
+//        if (!searchTerm.isEmpty()) {
+//            String lowerSearchTerm = searchTerm.toLowerCase();
+//            titleOrContentConditions = posts.title.lower().contains(lowerSearchTerm )
+//                    .or(posts.content.lower().contains(lowerSearchTerm));
+//        }
+
+//      검색어 문자열을 공백 기호 기준으로 다 split해서 배열로 만들고, 각 배열 요소에 담긴 키워드 조각들을 and한 결과가 게시물에 있으면 해당 게시물이 추출됨
+        if (!searchTerm.isEmpty()) { // 만약 문자열이 공백이 아니라면
+            String[] keywords = searchTerm.split("\\s+"); // 공백으로 분리한 검색어 배열 생성
+
+            // 각 단어를 처리하여 BooleanExpression 조건 생성
+            List<BooleanExpression> keywordConditions = Arrays.stream(keywords)
+                    .map(keyword -> posts.title.lower().like("%" + keyword.toLowerCase() + "%") // keyword가 포함된 게시물 title이 있으면 추출될 게시물로 선정
+                            .or(posts.content.lower().like("%" + keyword.toLowerCase() + "%"))) // keyword가 포함된 게시물 content가 있으면 추출될 게시물로 선정
+                    .collect(Collectors.toList()); // 각 키워드 조각들에 대해 title에 포함됨? content에 포함됨? 에 대한 조건을 모두 만들어 list로 만든다.
+
+            titleOrContentConditions = keywordConditions.stream()
+                    .reduce(BooleanExpression::and)
+                    .orElse(null); // 모든 조건이 없을 경우 null로 설정
+
+
+//             앞서 만든 검색어에 대한 모든 조건들을 and 연산하여 검색어 조건 생성 완료
+//             ex: 오늘 김밥 먹음 -> ('오늘'을 포함한 게시물 제목 or '오늘'을 포함한 게시물 컨텐츠)
+//                                    &&
+//                                  ('김밥'을 포함한 게시물 제목 or '김밥'을 포함한 게시물 컨텐츠)
+//                                    &&
+//                                  ('먹음'을 포함한 게시물 제목 or '먹음'을 포함한 게시물 컨텐츠)
+//            -----> 따라서 순서에 상관 없이 게시물 내용이나 제목에 '오늘', '김밥', '먹음'이 모두 포함된 게시물만 필터링되는 조건 완성
+        }
+
+        // '데이터'를 가져오는 쿼리
         JPAQuery<Posts> query = queryFactory.selectFrom(posts) // 게시물을 추출할 건데,
                 .join(posts.category, category) // 게시물을 카테고리와 조인한 형태로 가져올거임
-                .where(bannerConditions) // (where로 조건 추가 1.) 근데 조건은 이러하고 (밑에 있음)
-                .where(posts.postType.eq(PostType.valueOf("PROJECT"))) // (where로 조건 추가 2.) 게시물의 TYPE이 프로젝트인 것만 가져옴
-                .orderBy(sortOption.equals("nearDeadline") ? posts.endDate.asc() : posts.createdDate.desc());
+                .where(bannerConditions,posts.postType.eq(PostType.valueOf("PROJECT")));
+                // (where로 조건 추가 1.) 근데 조건은 이러하고 (밑에 있음)
+                // (where로 조건 추가 2.) 게시물의 TYPE이 프로젝트인 것만 가져옴
+
+        // 근데 검색어 관련 조건이 null이 아니라면, 검색어 관련 조건이 해당 쿼리문에 where절로 한번 더 엮임
+        if (titleOrContentConditions != null) {
+            query = query.where(titleOrContentConditions);
+        }
+
+        // 정렬 옵션에 따른 조건 추가
+        query=query.orderBy(sortOption.equals("nearDeadline") ? posts.endDate.asc() : posts.createdDate.desc());
                 //만약 소트 조건이 마감일순이면 마감일 순 정렬, 아니면 최신등록순 정렬
 
-        // 카운트 쿼리 별도로 보냄 (리팩토링 필요 예정 - 성능 최적화 위해)
+
+        // '카운트 쿼리' 별도로 보냄 (리팩토링 필요 예정 - 성능 최적화 위해)
         JPQLQuery<Posts> countQuery = queryFactory.selectFrom(posts)
                 .join(posts.category, category) // Join with category
-                .where(bannerConditions)
-                .where(posts.postType.eq(PostType.valueOf("PROJECT")));
-                // .orderBy(posts.createdDate.desc()); 카운트 쿼리에선 정렬 필요없음
+                .where(bannerConditions,posts.postType.eq(PostType.valueOf("PROJECT")));
+
+//              .orderBy(posts.createdDate.desc()); 카운트 쿼리에선 정렬 필요없음
+
+        // 근데 검색어 관련 조건이 null이 아니라면, 검색어 관련 조건이 해당 쿼리문에 where절로 한번 더 엮임
+        if (titleOrContentConditions != null) {
+            countQuery = countQuery.where(titleOrContentConditions);
+        }
+
 
         long total = countQuery.fetchCount(); // Count쿼리에 의해 전체 데이터 개수 알아냄
 
@@ -453,7 +507,7 @@ public class PostsService {
 
     }
 
-    // 프로젝트 게시물 조회에서 동적 쿼리의 where절에 들어갈 조건 생성하기
+    // 프로젝트 게시물 조회에서 선택된 배너[app,web,ai,game] 에 따라 동적 쿼리의 where절에 들어갈 조건 생성하기
     // 스터디는 카테고리에 추가 필드가 생길 여지가 있으므로 별도로 필터링 조건을 프로젝트 필터링과 구분하였음
     private BooleanExpression buildBannerConditionsInProjects(QCategory category, List<String> selectedBanners) {
 
@@ -523,16 +577,17 @@ public class PostsService {
         // 데이터를 가져오는 쿼리
         JPAQuery<Posts> query = queryFactory.selectFrom(posts) // 게시물을 추출할 건데,
                 .join(posts.category, category) // 게시물을 카테고리와 조인한 형태로 가져올거임
-                .where(bannerConditions) // (where로 조건 추가 1.) 근데 조건은 이러하고 (밑에 있음)
-                .where(posts.postType.eq(PostType.valueOf("STUDY"))) // (where로 조건 추가 2.) 게시물의 TYPE이 프로젝트인 것만 가져옴
+                .where(bannerConditions,posts.postType.eq(PostType.valueOf("STUDY")))
+                // (where로 조건 추가 1.) 근데 조건은 이러하고 (밑에 있음)
+                // (where로 조건 추가 2.) 게시물의 TYPE이 프로젝트인 것만 가져옴
                 .orderBy(sortOption.equals("nearDeadline") ? posts.endDate.asc() : posts.createdDate.desc());
         //만약 소트 조건이 마감일순이면 마감일 순 정렬, 아니면 최신등록순 정렬
 
         // 카운트 쿼리 별도로 보냄 (리팩토링 필요 예정 - 성능 최적화 위해)
         JPQLQuery<Posts> countQuery = queryFactory.selectFrom(posts)
                 .join(posts.category, category) // Join with category
-                .where(bannerConditions)
-                .where(posts.postType.eq(PostType.valueOf("STUDY")));
+                .where(bannerConditions,posts.postType.eq(PostType.valueOf("STUDY")));
+
         // .orderBy(posts.createdDate.desc()); 카운트 쿼리에선 정렬 필요없음
 
         long total = countQuery.fetchCount(); // Count쿼리에 의해 전체 데이터 개수 알아냄

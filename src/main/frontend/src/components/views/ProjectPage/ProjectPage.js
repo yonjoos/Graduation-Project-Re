4fcbdaf79d3df907f772101e6ba22f698f6c71b5
rilato <croco1997@naver.com>
2023-10-22 19:46:56 +0900

    fix categorize design

--- src/main/frontend/src/components/views/ProjectPage/ProjectPage.js
+++ src/main/frontend/src/components/views/ProjectPage/ProjectPage.js
@@ -285,7 +285,7 @@ function ProjectPage() {
                                         </Col>
                                     </Row>
                                     <Row>
-                                        분류: {item.web ? "Web " : ""}{item.app ? "App " : ""}{item.game ? "Game " : ""}{item.ai ? "AI " : ""}
+                                        {item.web ? "#Web " : ""}{item.app ? "#App " : ""}{item.game ? "#Game " : ""}{item.ai ? "#AI " : ""}
                                     </Row>
                                     <Divider></Divider>
                                     <Row>

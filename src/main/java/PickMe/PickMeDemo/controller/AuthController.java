e5e8d700ebd74a413eb7f56ec26fa2f44a472554
rilato <croco1997@naver.com>
2023-11-08 17:05:31 +0900

    fix duplicate nickname problem

--- src/main/java/PickMe/PickMeDemo/controller/AuthController.java
+++ src/main/java/PickMe/PickMeDemo/controller/AuthController.java
@@ -56,14 +56,24 @@ public class AuthController {
 
     // 닉네임 중복인 지에 대한 여부를 확인하는 컨트롤러 (회원가입 관련이므로 여기에 넣었음)
     @GetMapping("/nicknameDuplicate")
-    public ResponseEntity<NickNameDuplicateDto> checkNicknameAvailability(Principal principal, @RequestParam String nickname) {
+    public ResponseEntity<NickNameDuplicateDto> checkNicknameAvailability(@RequestParam String nickname) {
+
+        boolean isAvailable = userService.isNicknameAvailable(nickname);
+
+        //NickNameDuplicateDto에 isAvailable값을 실어서 반환
+        return ResponseEntity.ok(new NickNameDuplicateDto(isAvailable));
+    }
+
+    // 닉네임 중복인 지에 대한 여부를 확인하는 컨트롤러 (마이페이지 관련)
+    @GetMapping("/nicknameDuplicateString")
+    public ResponseEntity<NickNameDuplicateStringDto> checkNicknameAvailability(Principal principal, @RequestParam String nickname) {
         // 로그인 한 사람의 이메일 가져오기
         String email = principal.getName();
 
         // userService로부터 닉네임이 사용 가능한지를 알아옴 (isAvailable==true: 중복 아니어서 사용 가능 / isAvailable==false: 중복이어서 사용 불가능)
-        String isAvailable = userService.isNicknameAvailable(nickname, email);
+        String isAvailable = userService.isNicknameAvailableString(nickname, email);
 
         //NickNameDuplicateDto에 isAvailable값을 실어서 반환
-        return ResponseEntity.ok(new NickNameDuplicateDto(isAvailable));
+        return ResponseEntity.ok(new NickNameDuplicateStringDto(isAvailable));
     }
 }
